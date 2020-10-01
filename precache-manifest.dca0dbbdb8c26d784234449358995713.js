e99f1696d816f4be3274fdeb2962bb5e9a43df32
William Schutte <williamdschutte@gmail.com>
2020-10-01 11:48:20 -0400

    Updates

---
+++ precache-manifest.dca0dbbdb8c26d784234449358995713.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb5f557b5258850d892b03affc72ef82",
+    "url": "/react-around-auth/index.html"
+  },
+  {
+    "revision": "0a97bd2c8b1df6f685e0",
+    "url": "/react-around-auth/static/css/main.fbeccaa3.chunk.css"
+  },
+  {
+    "revision": "27c7f77205b40c524cf9",
+    "url": "/react-around-auth/static/js/2.f428b9e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-around-auth/static/js/2.f428b9e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a97bd2c8b1df6f685e0",
+    "url": "/react-around-auth/static/js/main.99142a67.chunk.js"
+  },
+  {
+    "revision": "4659e52b22ff7343b9b2",
+    "url": "/react-around-auth/static/js/runtime-main.dc4b9052.js"
+  },
+  {
+    "revision": "14a176339fc00af3ae93be979f1593cc",
+    "url": "/react-around-auth/static/media/Inter-Black.14a17633.woff2"
+  },
+  {
+    "revision": "b90562e0cf2dcfedca0b93ad7fa447d8",
+    "url": "/react-around-auth/static/media/Inter-Black.b90562e0.woff"
+  },
+  {
+    "revision": "001893789f7f342b520f29ac8af7d6ca",
+    "url": "/react-around-auth/static/media/Inter-Bold.00189378.woff"
+  },
+  {
+    "revision": "e7ae98681edfa1df7f1e3ebba0d4fb88",
+    "url": "/react-around-auth/static/media/Inter-Bold.e7ae9868.woff2"
+  },
+  {
+    "revision": "1e081edc16d92d42aeccec760174fbf4",
+    "url": "/react-around-auth/static/media/Inter-Regular.1e081edc.woff2"
+  },
+  {
+    "revision": "3ae6a7d3890c33d857fc00bd2e4c4820",
+    "url": "/react-around-auth/static/media/Inter-Regular.3ae6a7d3.woff"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "/react-around-auth/static/media/add-icon.b2fce4ac.svg"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "/react-around-auth/static/media/edit-icon.6bfdb354.svg"
+  },
+  {
+    "revision": "5df32a06fdf69f3ae19600017c36d45a",
+    "url": "/react-around-auth/static/media/exit-icon.5df32a06.svg"
+  },
+  {
+    "revision": "8f7611ae39c682f37516feec261f77dd",
+    "url": "/react-around-auth/static/media/header-logo.8f7611ae.svg"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "/react-around-auth/static/media/heart-icon-like.5bbb05f7.svg"
+  },
+  {
+    "revision": "a8acc871456b1001597672e02c5199ca",
+    "url": "/react-around-auth/static/media/heart-icon.a8acc871.svg"
+  },
+  {
+    "revision": "f202ad3053ae4410e6d246994df87581",
+    "url": "/react-around-auth/static/media/trash-icon.f202ad30.svg"
+  }
+]);
